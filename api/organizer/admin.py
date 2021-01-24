049dabfef44fec755c871bb6345f5639eebfe279
e.g. pavelka <pav@egpavelka.com>
2021-01-24 05:08:14 -0500

    simplify card and subtype relationships

--- api/organizer/admin.py
+++ api/organizer/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Card, NoteCard, LinkCard, Reference, Comment, Link, Activity
 
-# Register your models here.
+admin.site.register(Card)
+admin.site.register(NoteCard)
+admin.site.register(LinkCard)
+admin.site.register(Link)
+admin.site.register(Reference)
+admin.site.register(Comment)
+admin.site.register(Activity)
--- api/organizer/models.py
+++ api/organizer/models.py
@@ -1,52 +1,82 @@
 from django.db import models
 from django.conf import settings
 
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+
+
 class Card(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    item = models.ForeignKey('CardItem', on_delete=models.CASCADE, default=None)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                             on_delete=models.CASCADE)
     title = models.CharField(max_length=140, default=None)
     references = models.ManyToManyField('self',
                                         through='Reference',
                                         through_fields=('to_card', 'from_card'))
-    comments = models.ForeignKey('Comment', null=True, on_delete=models.CASCADE)
-    activities = models.ForeignKey('Activity', null=True, on_delete=models.CASCADE)
-    pass
+    def __str__(self):
+        return self.title
 
 
-class Reference(models.Model):
-    to_card = models.ForeignKey(Card, on_delete=models.CASCADE)
-    from_card = models.ForeignKey(Card, on_delete=models.CASCADE, related_name='referee')
-    pass
-
-class Comment(models.Model):
-    content = models.CharField(max_length=1200)
-    pass
-
-
-class CardItem(models.Model):
-    pass
-
-class Note(models.Model):
-    card = models.OneToOneField(CardItem, on_delete=models.CASCADE, primary_key=True)
+class NoteCard(models.Model):
+    card = models.OneToOneField(Card,
+                                default=None,
+                                primary_key=True,
+                                on_delete=models.CASCADE)
     content = models.TextField()
+
+    def __str__(self):
+        return self.content
+
+
+class LinkCard(models.Model):
+    card = models.OneToOneField(Card,
+                                default=None,
+                                primary_key=True,
+                                on_delete=models.CASCADE)
+    link = models.ForeignKey('Link',
+                             default=None,
+                             on_delete=models.CASCADE)
     pass
 
 
 class Link(models.Model):
-    card = models.OneToOneField(CardItem, on_delete=models.CASCADE, primary_key=True)
+    text = models.CharField(max_length=140)
     url = models.URLField()
     pass
+
+
+class Reference(models.Model):
+    to_card = models.ForeignKey(Card,
+                                on_delete=models.CASCADE)
+    from_card = models.ForeignKey(Card,
+                                  related_name='referrer',
+                                  on_delete=models.CASCADE)
+    pass
+
+
+class Comment(models.Model):
+    card = models.ForeignKey(Card,
+                             default=None,
+                             related_name='comments',
+                             on_delete=models.CASCADE)
+    content = models.CharField(max_length=1200)
+    pass
+
 
 class Activity(models.Model):
     # VERB_CHOICES = [('CREATED', 'created'),
     #                 ('UPDATED', 'updated',
     #                  'REFERENCED', 'referenced',
     #                  'INCLUDED')]
-    verb = models.CharField(max_length=140, default=None)
-    action = models.ForeignKey('ActionItem', on_delete=models.CASCADE, null=True)
+    verb = models.CharField(max_length=140, default='created')
+    action = models.ForeignKey('ActionItem',
+                               null=True,
+                               on_delete=models.CASCADE)
     timestamp = models.DateTimeField(auto_now_add=True)
-    # target = models.ForeignKey(Card, on_delete=models.CASCADE)
-    # target = ActionTarget (Card, Page, Realm) -- maybe through inverse
+    target = models.ForeignKey(Card,
+                               default=None,
+                               related_name='activities',
+                               on_delete=models.CASCADE)
     pass
 
 class ActionItem(models.Model):
